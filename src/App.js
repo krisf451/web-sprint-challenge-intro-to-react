1890db9296bda281aff6135fe3b9389146fe2302
Kristian Fulkerson <kristianf451@gmail.com>
2021-08-13 15:40:56 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  //set up state for the character data, starts as an empty array
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        console.log(res.data);
+        //set characters array to the data returned from the API
+        setCharacters(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS!!!!</h1>
+      <Character characters={characters} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,45 @@
 // Write your Character component here
+import React from "react";
+import {
+  Container,
+  Row,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Col
+} from "reactstrap";
+
+export default function Character(props) {
+  //   console.log(props.characters);
+  const { characters } = props;
+  //   console.log(characters);
+
+  return (
+    <Container>
+      <Row>
+        {characters.map((char, index) => {
+          return (
+            <Col xs="12" sm="6" md="4">
+              <Card>
+                <CardImg
+                  top
+                  width="100%"
+                  src="https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png?region=0%2C0%2C1200%2C1200"
+                  alt="starwars image"
+                />
+                <CardBody>
+                  <CardTitle>Name: {char.name}</CardTitle>
+                  <CardSubtitle>Birth Year: {char.birth_year}</CardSubtitle>
+                  <CardSubtitle>Height: {char.height}</CardSubtitle>
+                  <CardSubtitle>Mass: {char.mass}</CardSubtitle>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}

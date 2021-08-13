d79e9788891b3ad542b9c12f0bf39b0f17d95c17
Kristian Fulkerson <kristianf451@gmail.com>
2021-08-13 15:47:27 -0700

    test, mvp

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
         console.error(err);
       });
   }, []);
-
+  //test change
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
